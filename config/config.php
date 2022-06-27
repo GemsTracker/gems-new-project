70f0678f196de30a0799de5bf05eb85da35df908
Jasper van Gestel <jvangestel@gmail.com>
2022-06-27 15:24:38 +0200

    Load the app config in the end and add Gems\Dev configprovider

--- config/config.php
+++ config/config.php
@@ -25,10 +25,14 @@ $aggregator = new ConfigAggregator([
     \Mezzio\Twig\ConfigProvider::class,
     \Laminas\Diactoros\ConfigProvider::class,
 
-    // Default App module config
-    App\ConfigProvider::class,
+
+
     \Gems\ConfigProvider::class,
     \Gems\LegacyConfigProvider::class,
+    \Gems\Dev\ConfigProvider::class,
+
+    // Default App module config
+    App\ConfigProvider::class,
 
     // Load application config in a pre-defined order in such a way that local settings
     // overwrite global settings. (Loaded as first to last):
