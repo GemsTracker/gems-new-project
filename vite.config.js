582dd468d00ad6e6f62c3c537c2ed34d657827c3
Jasper van Gestel <jvangestel@gmail.com>
2022-09-29 12:02:38 +0200

    add gems-js for vue components

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,19 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from 'vite';
-import gems from './resource/vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import gems from './resource/vite-plugin';
 
 export default defineConfig({
-    plugins: [
-        gems({
-            input: [
-                //'resource/js/vue-respondent.js',
-                'resource/js/gems-vue.js',
-                'resource/js/jquery.js',
-            ],
-            refresh: true,
-            publicHost: 'gemstracker.test',
-        }),
-        vue(),
-    ],
+  plugins: [
+    gems({
+      input: [
+        // 'resource/js/vue-respondent.js',
+        'resource/js/gems-vue.js',
+        'resource/js/jquery.js',
+      ],
+      refresh: true,
+      publicHost: 'gemstracker.test',
+    }),
+    vue(),
+  ],
 });
