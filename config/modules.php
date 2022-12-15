4f1ef08aa0a76a8d8f0d6a3ad3379aa0f28650f3
Jasper van Gestel <jvangestel@gmail.com>
2022-12-16 00:11:30 +0100

    Add gems fhir api module

--- config/modules.php
+++ config/modules.php
@@ -6,6 +6,7 @@ return [
     \Gems\ConfigProvider::class,
     \Gems\LegacyConfigProvider::class,
     \Gems\Dev\ConfigProvider::class,
+    \Gems\Api\Fhir\ConfigProvider::class,
 
     // Default App module config
     App\ConfigProvider::class,
